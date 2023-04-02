9f0ca89398b0a97ba519ff580ea82c0426affa82
cinnamonmilk <trivialcve@proton.me>
2023-04-02 08:59:16 +0800

    fixed css

--- home-page.php
+++ home-page.php
@@ -5,19 +5,16 @@
     ?>
 <?php get_header();?>   <!--  Tells WordPress to include header.php -->
 <section class="container-fluid aboutbg text-center">
-            <div class="container">
-            
+            <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+            <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+            <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
+            <div class="container">           
 <div class="blocker"></div>
         </div> <!-- container -->
     </section>
 
     <section class="container-fluid articlesbg">
         <div class="container">
-        <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
-        <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
-        <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
-
-
 
             <h3>LATEST ARTICLES</h3>
             <div class="row">
